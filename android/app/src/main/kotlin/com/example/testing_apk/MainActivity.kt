b948f51a3b1e2977f8f63ad3b8564c377f277ad2
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2021-11-13 09:38:48 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/testing_apk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testing_apk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Profile")),
+      ),
+      body: Center(child: Text("Nahid Hasan Noyon")),
+    );
+  }
+}
